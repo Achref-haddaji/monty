ebf449bd60196dccb61eafab065b7a6102f711ce
Achref-haddaji <haddaji.achref19@gmail.com>
2022-05-12 16:27:06 +0100

    nop.c

--- nop.c
+++ nop.c
@@ -1,12 +1,13 @@
 #include "monty.h"
 /**
  * nop - do nothing
- * @stack: unused attribute
- * @line_number: unused attribute
+ * @stack: pointer to the top of the stack
+ * @line_number: the line number of the command interepted
  *
  * Return: void
  */
-void nop(stack_t **stack __attribute__((unused)),
-		 unsigned int line_number __attribute__((unused)))
+void nop(stack_t **stack, unsigned int line_number)
 {
+	(void) line_number;
+	(void) stack;
 }
