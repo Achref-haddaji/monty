f812869f30dd1ed02fecd5200f3369551171ac9f
Achref-haddaji <haddaji.achref19@gmail.com>
2022-05-12 14:24:28 +0100

    monty.c

--- monty.c
+++ monty.c
@@ -1,64 +1,44 @@
-
-  
 #include "monty.h"
-
-char *line;
 /**
- *main - entry point for interpretor
- *@argc: the number of command line arguments
- *@argv: the array of command line argument strings
+ * main - monty language interpreter
  *
- *Return: 0 always
+ * @argc: argument count
+ * @argv: argument value (FILENAME in this case)
+ * Return: Exit status
  */
 int main(int argc, char **argv)
 {
-	stack_t *stack = NULL;
-	int i, j;
-	FILE *file;
-	unsigned int line_number = 0;
-	char *opcode;
-	ssize_t read = 0;
-	size_t size;
+	char *line_buf = NULL;
+	size_t line_buf_size = 1;
+	int line_count = 0;
+	ssize_t line_size = 1;
+	FILE *fD;
+	char *FILENAME = NULL;
+	stack_t *my_stack = NULL;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "Usage: monty file\n");
+		return (EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
+	FILENAME = argv[1];
+	fD = fopen(FILENAME, "r");
+
+	if (!fD)
 	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
+		fprintf(stderr, "Error: Can't open file %s\n", FILENAME);
+		return (EXIT_FAILURE);
 	}
-	while (read != -1)
+	line_size = getline(&line_buf, &line_buf_size, fD);
+	while (line_size >= 0)
 	{
-		line = NULL;
-		size = 0;
-		i = 0;
-		j = 0;
-		read = getline(&line, &size, file);
-		line_number++;
-		while (line[j] == ' ')
-		{
-			j++;
-		}
-		while (line[i] != '\n' && line[i] != '\0')
-		{
-			i++;
-		}
-		if (line[j] == '\n' || line[j] == '#')
-			continue;
-		line[i] = '\0';
-		if (line[0] == '\0')
-			break;
-		opcode = get_opcode();
-		get_func(&stack, line_number, opcode);
-		free(opcode);
-		free(line);
+		line_count++;
+		parsecmd(&my_stack, line_count, line_buf);
+		line_size = getline(&line_buf, &line_buf_size, fD);
 	}
-	fclose(file);
-	free(line);
-	free_stack(stack);
-	return (0);
+	free(line_buf);
+	free_stack(&my_stack);
+	line_buf = NULL;
+	fclose(fD);
+	return (EXIT_SUCCESS);
 }
